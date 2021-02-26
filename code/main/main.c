7b7454d81f0754ac1853ab96d73e0f35c3b35c8f
Menno <my.bil@student.avans.nl>
2021-02-26 13:04:11 +0100

    [FEATURE] simple display code (NO COMPONENTS ADDED))

---
+++ code/main/main.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "driver/gpio.h"
+#include "driver/i2c.h"
+#include "esp_log.h"
+#include "sdkconfig.h"
+#include "esp32/rom/uart.h"
+
+#include "smbus.h"
+#include "i2c-lcd1602.h"
+
+#define TAG "app"
+
+// Undefine USE_STDIN if no stdin is available (e.g. no USB UART) - a fixed delay will occur instead of a wait for a keypress.
+//#define USE_STDIN  1
+#undef USE_STDIN
+
+#define I2C_MASTER_NUM           I2C_NUM_0
+#define I2C_MASTER_TX_BUF_LEN    0                     // disabled
+#define I2C_MASTER_RX_BUF_LEN    0                     // disabled
+#define I2C_MASTER_FREQ_HZ       100000
+#define I2C_MASTER_SDA_IO        CONFIG_I2C_MASTER_SDA
+#define I2C_MASTER_SCL_IO        CONFIG_I2C_MASTER_SCL
+#define LCD_NUM_ROWS			 4
+#define LCD_NUM_COLUMNS			 40
+#define LCD_NUM_VIS_COLUMNS		 20
+
+static void i2c_master_init(void)
+{
+    int i2c_master_port = I2C_MASTER_NUM;
+    i2c_config_t conf;
+    conf.mode = I2C_MODE_MASTER;
+    conf.sda_io_num = I2C_MASTER_SDA_IO;
+    conf.sda_pullup_en = GPIO_PULLUP_DISABLE;  // GY-2561 provides 10kΩ pullups
+    conf.scl_io_num = I2C_MASTER_SCL_IO;
+    conf.scl_pullup_en = GPIO_PULLUP_DISABLE;  // GY-2561 provides 10kΩ pullups
+    conf.master.clk_speed = I2C_MASTER_FREQ_HZ;
+    i2c_param_config(i2c_master_port, &conf);
+    i2c_driver_install(i2c_master_port, conf.mode,
+                       I2C_MASTER_RX_BUF_LEN,
+                       I2C_MASTER_TX_BUF_LEN, 0);
+}
+
+// WARNING: ESP32 does not support blocking input from stdin yet, so this polls
+// the UART and effectively hangs up the SDK.
+static uint8_t _wait_for_user(void)
+{
+    uint8_t c = 0;
+#ifdef USE_STDIN
+    while (!c)
+    {
+       STATUS s = uart_rx_one_char(&c);
+       if (s == OK) {
+          printf("%c", c);
+       }
+    }
+#else
+    vTaskDelay(1000 / portTICK_RATE_MS);
+#endif
+    return c;
+}
+
+void test(void *p) 
+{
+    // Set up I2C
+    i2c_master_init();
+    i2c_port_t i2c_num = I2C_MASTER_NUM;
+    uint8_t address = CONFIG_LCD1602_I2C_ADDRESS;
+
+    // Set up the SMBus
+    smbus_info_t * smbus_info = smbus_malloc();
+    smbus_init(smbus_info, i2c_num, address);
+    smbus_set_timeout(smbus_info, 1000 / portTICK_RATE_MS);
+
+    // Set up the LCD1602 device with backlight off
+    i2c_lcd1602_info_t * lcd_info = i2c_lcd1602_malloc();
+    i2c_lcd1602_init(lcd_info, smbus_info, true, LCD_NUM_ROWS, LCD_NUM_COLUMNS, LCD_NUM_VIS_COLUMNS);
+
+    ESP_LOGI(TAG, "cursor on");
+    _wait_for_user();
+    i2c_lcd1602_set_cursor(lcd_info, true);
+
+    ESP_LOGI(TAG, "write text at 0,0");
+    _wait_for_user();
+    i2c_lcd1602_move_cursor(lcd_info, 0, 0);
+    // i2c_lcd1602_write_char(lcd_info, 'H');
+    // i2c_lcd1602_move_cursor(lcd_info, 1, 0);
+    // i2c_lcd1602_write_char(lcd_info, 'O');
+    // i2c_lcd1602_move_cursor(lcd_info, 1, 0);
+    // i2c_lcd1602_write_char(lcd_info, 'I');
+    i2c_lcd1602_write_string(lcd_info, "Hoi");
+
+    while (1)
+    {
+        /* code */
+    }
+    
+}
+
+void app_main()
+{
+    xTaskCreate(&test, "lcd1602_task", 4096, NULL, 5, NULL);
+}
+
