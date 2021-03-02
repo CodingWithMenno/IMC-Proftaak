7cc4ad9a5925897ea5c9e51d1ea4a481e3e39e36
Menno <my.bil@student.avans.nl>
2021-03-02 08:10:39 +0100

    [IMPROVED] skeleton of structs and functions

--- code/components/esp32-lcdmenu/include/lcd-menu.h
+++ code/components/esp32-lcdmenu/include/lcd-menu.h
@@ -5,29 +5,34 @@
 #define LCD_MENU_OKE 0
 #define LCD_MENU_ERROR 1
 
+//Some constants for the lcd menu's
+#define TOTAL_MENUS 6
 #define MAX_ITEMS_ON_MENU 4
 #define MAX_LINES_ON_MENU 20
-#define MAX_MENU_POINTERS 3
 #define MAX_BUTTON_LENGTH 8
+
+//The current menu
+extern LCD_MENU *currentLcdMenu;
 
 //A struct for a menu item
 typedef struct
 {
     unsigned int id;
-    char *text[MAX_BUTTON_LENGTH];
+    char text[MAX_BUTTON_LENGTH];
     void (*onClick)(void);
+    int xCoord;
+    int yCoord;
 } LCD_MENU_ITEM;
 
-//A struct for a menu item
+//A struct for a menu
 typedef struct menu
 {
     unsigned int id;
-    unsigned int menuPointers[MAX_MENU_POINTERS];
-    char *text[MAX_LINES_ON_MENU];
+    char text[MAX_LINES_ON_MENU];
     void (*menuInit)(void);
     void (*menuExit)(void);
-    LCD_MENU_ITEM *menuItems[MAX_ITEMS_ON_MENU];
-    //struct menu *subMenu[];
+    unsigned int menuPointers[MAX_ITEMS_ON_MENU]; //Points to the id of the next possible menu's
+    LCD_MENU_ITEM *menuItems[MAX_ITEMS_ON_MENU]; //Points to all the menu items in this menu
 } LCD_MENU;
 
 
@@ -39,12 +44,19 @@ Call this method to init all the menu's
 int menu_initMenus();
 
 /*
-Function to switch between menu's
+Function to go back to the previous menu
 
 -Returns:       a LCD_MENU error code
--Parameters:    ID of next menu
 */
-int menu_switchMenu(unsigned int);
+int menu_goBackAMenu();
+
+/*
+Function to start the action (the onClick() function) of
+the current selected item on the current menu
+
+-Returns:       A LCD_MENU error code
+*/
+int menu_doActionCurrentItem();
 
 /*
 Function to set the cursor to the next item in the menu
@@ -59,5 +71,17 @@ Function to set the cursor to the previous item in the menu
 -Returns:       a LCD_MENU error code
 */
 int menu_goToPreviousitem();
+
+
+
+//OPTIONAL FUNCTIONS
+
+/*
+Function to switch between menu's
+
+-Returns:       a LCD_MENU error code
+-Parameters:    ID of next menu
+*/
+int menu_switchMenu(unsigned int);
 
 #endif
--- code/components/esp32-lcdmenu/lcd-menu.c
+++ code/components/esp32-lcdmenu/lcd-menu.c
@@ -2,5 +2,20 @@
 #include "i2c-lcd1602.h"
 #include "lcd-menu.h"
 
-static unsigned int currentMenu;
+//Extern variable to the current lcd menu
+LCD_MENU *currentLcdMenu;
+
+//All the menu's in the application
+static LCD_MENU *lcdMenus[TOTAL_MENUS];
+//The current selected menu item in the current menu
 static unsigned int currentMenuItem;
+
+
+int menu_initMenus()
+{
+    //Reserve memory for all the menu's
+    //Init all the menu's with the right value's
+    //Store all the menu's in the lcdMenus array
+    //Set the currentLcdMenu and currentMenuItem
+}
+
