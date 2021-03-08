b3879d7c9c9f6d9006305e77a610c9e8572d1b5f
sebas <mab.tromper@student.avans.nl>
2021-03-08 13:35:33 +0100

    Set brackets correct in radioSwitch.c

--- code/components/esp32-radio/radioSwitch.c
+++ code/components/esp32-radio/radioSwitch.c
@@ -13,11 +13,15 @@ void radioSwitch(char channel[])
         Ip = "https://20103.live.streamtheworld.com/TLPSTR09.mp3";
         Ipa = *Ip;
         startRadio(Ipa);
-    }else if(strcmp(channel, "Q") == 0){
+    }
+    else if(strcmp(channel, "Q") == 0)
+    {
         Ip = "https://icecast-qmusicnl-cdp.triple-it.nl/Qmusic_nl_live_96.mp3";
         Ipa = *Ip;
         startRadio(Ipa);
-    }else if (strcmp(channel, "SKY") == 0){
+    }
+    else if (strcmp(channel, "SKY") == 0)
+    {
         Ip = "https://19993.live.streamtheworld.com/SKYRADIO.mp3";
         Ipa = *Ip;
         startRadio(Ipa);
